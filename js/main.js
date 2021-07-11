41852f55b885cfaf8342a9ee8a094f150848e567
fatimakhatungit <fatimakhatuncse@gmail.com>
2021-07-12 00:11:01 +0600

    added

--- js/main.js
+++ js/main.js
@@ -9,50 +9,50 @@
         });
 
         // brand-active
-    $('.testimonial-active').slick({
-        dots: false,
-        arrows: true,
-        infinite: true,
-        speed: 300,
-        autoplay:true,
-        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1300,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-            }
-          },
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 991,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
+//     $('.testimonial-active').slick({
+//         dots: false,
+//         arrows: true,
+//         infinite: true,
+//         speed: 300,
+//         autoplay:true,
+//         prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+//         nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         responsive: [
+//           {
+//             breakpoint: 1300,
+//             settings: {
+//               slidesToShow: 1,
+//               slidesToScroll: 1,
+//               infinite: true,
+//             }
+//           },
+//           {
+//             breakpoint: 1024,
+//             settings: {
+//               slidesToShow: 1,
+//               slidesToScroll: 1
+//             }
+//           },
+//           {
+//             breakpoint: 991,
+//             settings: {
+//               slidesToShow: 1,
+//               slidesToScroll: 1
+//             }
+//           },
+//           {
+//             breakpoint: 480,
+//             settings: {
+//               slidesToShow: 1,
+//               slidesToScroll: 1
+//             }
+//           }
+//         ]
+//       });
 		
 
-	});
+// 	});
 
 }(jQuery));
