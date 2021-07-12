55dded6d95c1a108cd0458df14daef94d36f073e
fatimakhatungit <fatimakhatuncse@gmail.com>
2021-07-12 20:15:12 +0600

    added

--- js/main.js
+++ js/main.js
@@ -2,60 +2,13 @@
 
 	jQuery(document).ready(function(){
 
-
-      
-
-        // testimonial-active
-    // $('.images-active').slick({
-    //     dots: false,
-    //     arrows: true,
-    //     infinite: true,
-    //     speed: 300,
-    //     autoplay:true,
-    //     prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
-    //     nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     responsive: [
-    //       {
-    //         breakpoint: 1300,
-    //         settings: {
-    //           slidesToShow: 1,
-    //           slidesToScroll: 1,
-    //           infinite: true,
-    //         }
-    //       },
-    //       {
-    //         breakpoint: 1024,
-    //         settings: {
-    //           slidesToShow: 1,
-    //           slidesToScroll: 1
-    //         }
-    //       },
-    //       {
-    //         breakpoint: 991,
-    //         settings: {
-    //           slidesToShow: 1,
-    //           slidesToScroll: 1
-    //         }
-    //       },
-    //       {
-    //         breakpoint: 480,
-    //         settings: {
-    //           slidesToShow: 1,
-    //           slidesToScroll: 1
-    //         }
-    //       }
-    //     ]
-    //   });
-
-     // testimonial-active
+     // slider-active
      $('.slider-active').slick({
       autoplay: true,
       dots: false,
       arrows: false,
       infinite: true,
-      speed: 300,
+      speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
   });
