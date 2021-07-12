e796f568b79d367ca47bf2b3d46f15ca156163f3
fatimakhatungit <fatimakhatuncse@gmail.com>
2021-07-12 18:17:16 +0600

    added

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
       
 
         // testimonial-active
-    // $('.testimonial-active').slick({
+    // $('.images-active').slick({
     //     dots: false,
     //     arrows: true,
     //     infinite: true,
@@ -49,8 +49,55 @@
     //     ]
     //   });
 
+     // testimonial-active
+     $('.slider-active').slick({
+      autoplay: true,
+      dots: false,
+      arrows: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+  });
+
+  $('.testimonial-active').slick({
+    autoplay: true,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
     // service - active
 $('.testimonial-active').slick({
+  autoplay: true,
   dots: false,
   arrows: true,
   infinite: true,
