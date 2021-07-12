7914830752e56b0c268e61467d26d2391ae91b02
fatimakhatungit <fatimakhatuncse@gmail.com>
2021-07-12 11:30:14 +0600

    added

--- js/main.js
+++ js/main.js
@@ -6,48 +6,85 @@
       
 
         // testimonial-active
-    $('.testimonial-active').slick({
-        dots: false,
-        arrows: true,
-        infinite: true,
-        speed: 300,
-        autoplay:true,
-        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+    // $('.testimonial-active').slick({
+    //     dots: false,
+    //     arrows: true,
+    //     infinite: true,
+    //     speed: 300,
+    //     autoplay:true,
+    //     prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+    //     nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     responsive: [
+    //       {
+    //         breakpoint: 1300,
+    //         settings: {
+    //           slidesToShow: 1,
+    //           slidesToScroll: 1,
+    //           infinite: true,
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 1024,
+    //         settings: {
+    //           slidesToShow: 1,
+    //           slidesToScroll: 1
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 991,
+    //         settings: {
+    //           slidesToShow: 1,
+    //           slidesToScroll: 1
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 480,
+    //         settings: {
+    //           slidesToShow: 1,
+    //           slidesToScroll: 1
+    //         }
+    //       }
+    //     ]
+    //   });
+
+    // service - active
+$('.testimonial-active').slick({
+  dots: true,
+  arrows: true,
+  infinite: true,
+  speed: 300,
+  prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+  nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1300,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-            }
-          },
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 991,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
 		
 
 	});
